9ec402be4f3397235ec2e444610d7d1db02cf439
GoracioNewport <bdfyljkub@gmail.com>
2023-01-12 22:22:52 +0600

    Project restructure

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
 """This module is responsible with all database interactions"""
 from tinydb import TinyDB, Query
 
-db = TinyDB("./database.json)
+db = TinyDB("./database.json")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from config import API_KEY
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -9,10 +9,11 @@ logging.basicConfig(
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    if update.effective_chat != None:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(os.getenv('API_KEY')).build()
+    application = ApplicationBuilder().token(str(API_KEY)).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
